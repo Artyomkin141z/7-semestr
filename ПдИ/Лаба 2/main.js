ded0a819acf52b6d39a415b7d460075443f14c5d
Artyomkin141z <gilimov2artyom@gmail.com>
2022-09-02 21:34:01 +0300

    first commit

---
+++ ПдИ/Лаба 2/main.js
@@ -0,0 +1,47 @@
+function checkAge1(age){
+    return age >= 18 ? true : 'Родители разрешили?';
+}
+
+function checkAge2(age){
+    return age >= 18 || 'Родители разрешили?';
+}
+
+function min(a, b){
+    return a < b ? a : b;
+}
+
+function pow(x, n){
+    return x**n;
+}
+
+function les4(a){
+    if(a % 2 == 1) alert(a);
+    while(a % 2 === 0){
+        if(a % 2 == 1) alert(a);
+        else a = prompt('Введите число');
+    }    
+}
+
+function les5(str){
+    if(+str){
+        alert(+str * 2);
+    }
+    console.log(str * 2);
+    while(isNaN(+str)){
+        str =  prompt('Введите строку');
+        if(+str){
+            alert(+str * 2);
+            break;
+        }
+    } 
+}
+
+function les6(mass){
+    let answer = [];
+    for(let i = 0; i< mass.length; i++){
+        if(mass[i] % 2 === 0){
+            answer.push(mass[i]);
+        }
+    }
+    console.log(answer);
+}
---
+++ ПнП.NET/ConsoleHello/ConsoleHello/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace ConsoleHello
+{ // создаваемое пространство имен
+    class Program
+    { // имя класса по умолчанию
+        static void Main(string[] args)
+        {
+            int[,] points = { { 2, -2 }, { 3, -22 }, { 0, 4 } };
+            les2(points);
+        }
+
+        public static void les2(int[,] mass)
+        {
+            int X1 = 0, Y1 = 0, X2 = 0, Y2 = 0, distance = 0;
+            int[,] answer;
+
+            for (int i = 0; i < mass.GetLength(0); i++)
+            {
+                int x1 = mass[i, 0];
+                int y1 = mass[i, 1];
+                for(int j = 0; j < mass.GetLength(0); j++)
+                {
+                    int x2 = mass[j, 0];
+                    int y2 = mass[j, 1];
+
+                    int localDistance = (int) Math.Sqrt(
+                        Math.Pow(Convert.ToDouble(x2 - x1), 2) + 
+                        Math.Pow(Convert.ToDouble(y2 - y1), 2)
+                        );
+                    
+
+                    if (localDistance > distance)
+                    {
+                        distance = localDistance;   
+                        X1 = x1;
+                        Y1 = y1;
+                        X2 = x2;
+                        Y2 = y2;
+                    }
+                }
+            }
+            Console.WriteLine("Lesson1 1");
+            Console.WriteLine("Maximum distance between points: " + distance.ToString());
+            Console.WriteLine("Points: ("
+            + X1.ToString() + ", "
+            + Y1.ToString() + ") and ("
+            + X2.ToString() + ", " 
+            + Y2.ToString() + ")");
+        }
+
+        public static void les3()
+        {
+
+        }
+    }
+}
