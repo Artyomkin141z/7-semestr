8f8ea15f502452136c31bfe0b74f0816953682ce
Artyomkin141z <gilimov2artyom@gmail.com>
2022-09-13 20:26:30 +0300

    1

---
+++ РКПП/laba4/src/laba/paterns/Singleton.java
@@ -0,0 +1,21 @@
+package laba.paterns;
+
+public class ST {
+    class Singleton {
+        private static Singleton instance = null;
+        private Singleton() {}
+        public static Singleton getInstance() {
+            if (instance == null) {
+                instance = new Singleton();
+            }
+            return instance;
+        }
+        public void setUp() {
+            System.out.println("setUp");
+        }
+    }
+    public void test(){
+        Singleton singleton = Singleton.getInstance();
+        singleton.setUp();
+    }
+}
