0131a255b5316cf4711e147179318172af26ba70
Artyomkin141z <gilimov2artyom@gmail.com>
2022-10-13 12:59:35 +0300

    1

--- РКПП/laba5_calculator/src/Calculation.java
+++ РКПП/laba5_calculator/src/Calculation.java
@@ -1,2 +1,54 @@
-package PACKAGE_NAME;public class Calculation {
+import java.util.ArrayList;
+
+public class Calculation {
+    String strNumber = "";
+    Boolean isDot = false;
+    String lastOperator = "";
+    String X = "", Y = "";
+    String answer = "", strExample = "";
+
+
+    public String getAnswer(){
+        saveNumber();
+        this.answer = caclulateNumber();
+        return this.answer;
+    }
+    public String getStrExample(){
+        this.strExample = this.X + " " + this.lastOperator + " " + this.Y + " =";
+        return this.strNumber;
+    }
+    public String getStrNumber(){return this.strNumber;}
+    public void setLastOperator(String value){
+        saveNumber();
+        this.lastOperator = value;
+    }
+    public void saveNumber(){
+        if(strNumber.toCharArray()[strNumber.length()-1] == '.'){
+            strNumber += "0";
+        }
+        else if(strNumber.indexOf(".") == -1){
+            strNumber += ".0";
+        }
+        if(X == ""){
+            this.X = strNumber;
+        }
+        else {
+            this.X = caclulateNumber();
+            this.Y = strNumber;
+        }
+        this.strNumber = "";
+    }
+    public void printStrNumber(String symbol){
+        strNumber += symbol;
+    }
+
+    public String caclulateNumber(){
+        double x = Double.parseDouble(this.X);
+        double y = Double.parseDouble(this.Y);
+        double a = 0.0;
+        if(lastOperator == "*"){
+            a = x * y;
+        }
+        return Double.toString(a);
+    }
 }
--- РКПП/laba5_calculator/src/Calculator.java
+++ РКПП/laba5_calculator/src/Calculator.java
@@ -33,13 +33,17 @@ public class Calculator extends JFrame implements ActionListener {
     private JButton operSign;//Знак числа
 
 
+    private Calculation c;
     Calculator(){
+        c = new Calculation();
+
         this.setTitle("Калькулятор (Лаба 5)");
-        this.setSize(500, 400);
+        //this.setSize(1000, 1000);
+        this.setResizable(false);
         Container c = this.getContentPane();
         c.setLayout(new BorderLayout());
 
-        JPanel calcPanel = new JPanel(new GridLayout(3, 1));
+        JPanel calcPanel = new JPanel(new GridLayout(2, 1));
         this.getContentPane().add(calcPanel, "North");
 
         // Пример
@@ -49,30 +53,39 @@ public class Calculator extends JFrame implements ActionListener {
         this.inputJT.setBackground(Color.white);
 
         // Пример
-        this.numberJT = new JTextField("");
+        this.numberJT = new JTextField("0");
         this.numberJT.setHorizontalAlignment(4);
         this.numberJT.setEditable(false);
         this.numberJT.setBackground(Color.white);
 
-        JPanel oprationsJP = new JPanel(new GridLayout(6, 4));
-        this.getContentPane().add(oprationsJP, "Center");
+        JPanel operationsJP = new JPanel(new GridLayout(6, 4));
+        this.getContentPane().add(calcPanel, "North");
+        this.getContentPane().add(operationsJP, "Center");
         Font displayFont = new Font("Calibri", 2, 20);
 
         this.numberJT.setFont(displayFont);
         this.inputJT.setFont(displayFont);
 
-
-
         this.num1 = new JButton("1");
+        this.num1.setActionCommand("1");
         this.num2 = new JButton("2");
+        this.num2.setActionCommand("2");
         this.num3 = new JButton("3");
+        this.num3.setActionCommand("3");
         this.num4 = new JButton("4");
+        this.num4.setActionCommand("4");
         this.num5 = new JButton("5");
+        this.num5.setActionCommand("5");
         this.num6 = new JButton("6");
+        this.num6.setActionCommand("6");
         this.num7 = new JButton("7");
+        this.num7.setActionCommand("7");
         this.num8 = new JButton("8");
+        this.num8.setActionCommand("8");
         this.num9 = new JButton("9");
+        this.num9.setActionCommand("9");
         this.num0 = new JButton("0");
+        this.num0.setActionCommand("0");
         this.num1.addActionListener(this);
         this.num2.addActionListener(this);
         this.num3.addActionListener(this);
@@ -85,19 +98,33 @@ public class Calculator extends JFrame implements ActionListener {
         this.num0.addActionListener(this);
 
         this.operProc = new JButton("%");
+        this.operProc.setActionCommand("%");
         this.operCE = new JButton("CE");
+        this.operCE.setActionCommand("CE");
         this.operC = new JButton("C");
+        this.operC.setActionCommand("C");
         this.operErase = new JButton("<-");
+        this.operErase.setActionCommand("<-");
         this.operFraction = new JButton("1/x");
+        this.operFraction.setActionCommand("1/x");
         this.operSquare = new JButton("x^2");
+        this.operSquare.setActionCommand("x^2");
         this.operRoot = new JButton("sqr");
+        this.operRoot.setActionCommand("sqr");
         this.operDevide = new JButton("/");
+        this.operDevide.setActionCommand("/");
         this.operMultiply = new JButton("*");
+        this.operMultiply.setActionCommand("*");
         this.operMinus = new JButton("-");
+        this.operMinus.setActionCommand("-");
         this.operPlus = new JButton("+");
+        this.operPlus.setActionCommand("+");
         this.operDot = new JButton(".");
+        this.operDot.setActionCommand(".");
         this.operEarly = new JButton("=");
+        this.operEarly.setActionCommand("=");
         this.operSign = new JButton("+/-");
+        this.operSign.setActionCommand("+/-");
         this.operProc.addActionListener(this);
         this.operCE.addActionListener(this);
         this.operC.addActionListener(this);
@@ -115,43 +142,120 @@ public class Calculator extends JFrame implements ActionListener {
 
         calcPanel.add(this.inputJT);
         calcPanel.add(this.numberJT);
-        calcPanel.add(oprationsJP);
+        //calcPanel.add(operationsJP);
 
-        oprationsJP.add(this.operProc);
-        oprationsJP.add(this.operCE);
-        oprationsJP.add(this.operC);
-        oprationsJP.add(this.operErase);
+        operationsJP.add(this.operProc);
+        operationsJP.add(this.operCE);
+        operationsJP.add(this.operC);
+        operationsJP.add(this.operErase);
 
-        oprationsJP.add(this.operFraction);
-        oprationsJP.add(this.operSquare);
-        oprationsJP.add(this.operRoot);
-        oprationsJP.add(this.operDevide);
+        operationsJP.add(this.operFraction);
+        operationsJP.add(this.operSquare);
+        operationsJP.add(this.operRoot);
+        operationsJP.add(this.operDevide);
 
-        oprationsJP.add(this.num7);
-        oprationsJP.add(this.num8);
-        oprationsJP.add(this.num9);
-        oprationsJP.add(this.operMultiply);
+        operationsJP.add(this.num7);
+        operationsJP.add(this.num8);
+        operationsJP.add(this.num9);
+        operationsJP.add(this.operMultiply);
 
-        oprationsJP.add(this.num4);
-        oprationsJP.add(this.num5);
-        oprationsJP.add(this.num6);
-        oprationsJP.add(this.operMinus);
+        operationsJP.add(this.num4);
+        operationsJP.add(this.num5);
+        operationsJP.add(this.num6);
+        operationsJP.add(this.operMinus);
 
-        oprationsJP.add(this.num1);
-        oprationsJP.add(this.num2);
-        oprationsJP.add(this.num3);
-        oprationsJP.add(this.operPlus);
+        operationsJP.add(this.num1);
+        operationsJP.add(this.num2);
+        operationsJP.add(this.num3);
+        operationsJP.add(this.operPlus);
 
-        oprationsJP.add(this.operSign);
-        oprationsJP.add(this.num0);
-        oprationsJP.add(this.operDot);
-        oprationsJP.add(this.operEarly);
+        operationsJP.add(this.operSign);
+        operationsJP.add(this.num0);
+        operationsJP.add(this.operDot);
+        operationsJP.add(this.operEarly);
 
         this.pack();
         this.setVisible(true);
         this.setDefaultCloseOperation(3);
     }
     public void actionPerformed(ActionEvent e) {
-        int a = 0;
+        String command = e.getActionCommand();
+        switch (command){
+            case "0":
+                c.printStrNumber("0");
+                numberJT.setText(c.getStrNumber());
+                break;
+            case "1":
+                c.printStrNumber("1");
+                numberJT.setText(c.getStrNumber());
+                break;
+            case "2":
+                c.printStrNumber("2");
+                numberJT.setText(c.getStrNumber());
+                break;
+            case "3":
+                c.printStrNumber("3");
+                numberJT.setText(c.getStrNumber());
+                break;
+            case "4":
+                c.printStrNumber("4");
+                numberJT.setText(c.getStrNumber());
+                break;
+            case "5":
+                c.printStrNumber("5");
+                numberJT.setText(c.getStrNumber());
+                break;
+            case "6":
+                c.printStrNumber("6");
+                numberJT.setText(c.getStrNumber());
+                break;
+            case "7":
+                c.printStrNumber("7");
+                numberJT.setText(c.getStrNumber());
+                break;
+            case "8":
+                c.printStrNumber("8");
+                numberJT.setText(c.getStrNumber());
+                break;
+            case "9":
+                c.printStrNumber("9");
+                numberJT.setText(c.getStrNumber());
+                break;
+            case "%":
+                break;
+            case "CE":
+                break;
+            case "C":
+                break;
+            case "<-":
+                break;
+            case "1/x":
+                break;
+            case "x^2":
+                break;
+            case "sqr":
+                break;
+            case "/":
+                c.setLastOperator("/");
+                break;
+            case "*":
+                c.setLastOperator("*");
+                break;
+            case "-":
+                c.setLastOperator("-");
+                break;
+            case "+":
+                c.setLastOperator("+");
+                break;
+            case ".":
+                break;
+            case "=":
+                //c.addNumberHelp();
+                inputJT.setText(c.getStrExample());
+                numberJT.setText(c.getAnswer());
+                break;
+            case "+/-":
+                break;
+        }
     }
 }
